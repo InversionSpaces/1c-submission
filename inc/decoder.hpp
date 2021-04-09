81ea47f252514acd71a57ff583ac8e631ffa8c17
Воробьев Михаил <inversionspaces@vivaldi.net>
2021-04-09 20:09:25 +0300

    added decoder logic

--- inc/decoder.hpp
+++ inc/decoder.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <list>
 #include <cctype>
 
 class Tokenizer {
@@ -28,5 +29,52 @@ public:
 };
 
 class Decoder {
+    using chunk_t = std::list<std::string>;
 
+    Tokenizer tokenizer;
+
+    void process_chunk(const chunk_t& chunk, const size_t position) const {
+        std::string code;
+
+        size_t pos = 0;
+        for (const auto& token: chunk) {
+            if (pos >= token.size())
+                break;
+            code.push_back(token.at(pos));
+            ++pos;
+        }
+
+        std::cout << code << " -> " << position << std::endl;
+    }
+
+public:
+
+    static const size_t CHUNK_SIZE = 5;
+
+    Decoder(std::istream& stream) : tokenizer(stream) {}
+
+    void process() {
+        chunk_t chunk;
+
+        std::string token;
+        for (size_t size = 0; size < CHUNK_SIZE; ++size) {
+            if (!tokenizer.get_token(token)) {
+                process_chunk(chunk, 0);
+                return;
+            }
+
+            chunk.emplace_back(std::move(token));
+        }
+
+        process_chunk(chunk, 0);
+
+        size_t position = 0;
+        while (tokenizer.get_token(token)) {
+            chunk.emplace_back(std::move(token));
+            chunk.pop_front();
+
+            ++position;
+            process_chunk(chunk, position);
+        }
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include <decoder.hpp>
 
 int main() {
-    Tokenizer tokenizer(std::cin);
-    std::string token;
-    while (tokenizer.get_token(token))
-        std::cout << token << std::endl;
+    Decoder decoder(std::cin);
+    decoder.process();
 }
